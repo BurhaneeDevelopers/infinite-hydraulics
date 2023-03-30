c55a690019ceeb3258058c345a5bf506fbc250e9
Mohammed <86866415+BurhaneeDevelopers@users.noreply.github.com>
2023-03-30 13:19:26 +0530

    Created product page and added functionality

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*"],
+  content: [
+    "*",
+    "./Pages/**/*.{html,js}",
+  ],
   theme: {
     extend: {},
+    screens: {
+      xxs: "350px",
+      xs: "500px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: false,
+  },
 }
